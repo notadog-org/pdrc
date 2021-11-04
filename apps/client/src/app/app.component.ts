940ab68dfe596600f608c1d05c5e3750d793c13c
Sergey <sperezhogin@griddynamics.com>
2021-11-04 16:03:14 +0100

    angular app is added

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'pdrc-offline-first-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}
