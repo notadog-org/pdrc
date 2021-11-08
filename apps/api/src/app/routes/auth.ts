16f4590d575c20b03107c6ee0d34213daddf5525
Sergey <sperezhogin@griddynamics.com>
2021-11-08 12:01:29 +0100

    jwt auth flow is implemented

--- apps/api/src/app/routes/auth.ts
+++ apps/api/src/app/routes/auth.ts
@@ -1,42 +1,55 @@
-import * as request from 'request';
 import * as express from 'express';
+import * as jwt from 'jsonwebtoken';
+import * as btoa from 'btoa';
+import axios from 'axios';
 
-import { COUCH_DB_ROOT_HOST, COUCH_DB_HOST } from '../../env';
+import { JWT_SECRET, COUCH_DB_ROOT_HOST, COUCH_DB_HOST } from '../../env';
 
 export const router = express.Router();
 
-router.post('/api/auth/register', async function (req, res) {
-  request
-    .put(`${COUCH_DB_ROOT_HOST}/_users/org.couchdb.user:${req.body.username}`, {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: {
-        name: req.body.username,
-        password: req.body.password,
+router.post('/api/auth/register', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const { data } = await axios.put(
+      `${COUCH_DB_ROOT_HOST}/_users/org.couchdb.user:${req.body.username}`,
+      {
+        name: username,
+        password,
         roles: [],
         type: 'user',
       },
-      json: true,
-    })
-    .pipe(res);
+      {}
+    );
+
+    const token = jwt.sign(
+      { sub: username },
+      Buffer.from(JWT_SECRET, 'base64')
+    );
+
+    return res.json({ token: `Bearer ${token}`, ...data });
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.post('/api/auth/login', async function (req, res) {
-  request
-    .post(`${COUCH_DB_HOST}/_session`, {
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: {
-        name: req.body.username,
-        password: req.body.password,
-      },
-      json: true,
-    })
-    .pipe(res);
+router.post('/api/auth/login', async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+
+    const { data } = await axios.get(`${COUCH_DB_HOST}/_session`, {
+      headers: { Authorization: `Basic ${btoa(`${username}:${password}`)}` },
+    });
+
+    const token = jwt.sign(
+      { sub: username },
+      Buffer.from(JWT_SECRET, 'base64')
+    );
+
+    return res.json({ token: `Bearer ${token}`, ...data });
+  } catch (err) {
+    next(err);
+  }
 });
 
 export const authRouter = router;
--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -1,3 +1,5 @@
+export const JWT_SECRET = process.env.NX_JWT_SECRET;
+
 export const COUCH_DB_HOST = process.env.NX_COUCH_DB_HOST;
 export const COUCH_DB_USER = process.env.NX_COUCH_DB_USER;
 export const COUCH_DB_PASSWORD = process.env.NX_COUCH_DB_PASSWORD;
