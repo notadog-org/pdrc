734677066bae487b62340441ed429ea4a8ab02cb
Sergey <sperezhogin@griddynamics.com>
2021-11-19 02:13:05 +0100

    order fields validation is added

--- apps/api/src/app/design/index.spec.ts
+++ apps/api/src/app/design/index.spec.ts
@@ -1,14 +1,172 @@
+import { fail } from 'assert';
 import { validate_doc_update } from './index';
+
+// fields
+const CATEGORY_FIELD = 'category';
+const DATE_FIELD = 'date';
+const ITEMS_FIELD = 'items';
+
+const CAR_MODEL_FIELD = 'carModel';
+const CAR_PRODUCER_FIELD = 'carProducer';
+const CLIENT_NAME_FIELD = 'clientName';
+const CLIENT_PHONE_FIELD = 'clientPhone';
+
+const ORDER_ITEM_CAR_CLASS_FIELD = 'carClass';
+const ORDER_ITEM_COUNT_FIELD = 'count';
+const ORDER_ITEM_PART_FIELD = 'part';
+const ORDER_ITEM_SIZE_FIELD = 'size';
+const ORDER_ITEM_TABLE_FIELD = 'table';
+const ORDER_ITEM_PRICE_FIELD = 'price';
+
+// values
+const CATEGORY_VALID_VALUES = [];
+const CAR_CLASS_VALID_VALUES = [];
+const PART_VALID_VALUES = [];
+const SIZE_VALID_VALUES = [];
 
 describe('Base validation', () => {
   describe('order', () => {
-    test('error if empty title', () => {
-      const newDoc = { title: '', type: 'order' };
+    const newDoc = {
+      type: 'order',
+      carModel: 'A5',
+      carProducer: 'Audi',
+      category: 1,
+      clientName: 'Ivan',
+      clientPhone: '89998887766',
+      date: new Date(),
+      items: [
+        {
+          carClass: 'A',
+          count: 1,
+          part: 'right door',
+          size: '1-2',
+          table: 'Complicated',
+          price: 200,
+        },
+        {
+          carClass: 'A',
+          count: 1,
+          part: 'right door',
+          size: '1-2',
+          table: 'Simple',
+          price: 200,
+        },
+      ],
+    };
+
+    test('is valid', () => {
+      expect(validate_doc_update(newDoc, {}, {})).toBeTruthy();
+    });
+
+    test.each([CATEGORY_FIELD, DATE_FIELD, ITEMS_FIELD])(
+      'error if %s field is not defined',
+      async (key) => {
+        const doc = { ...newDoc };
+        delete doc[key];
+
+        try {
+          validate_doc_update(doc, {}, {});
+          fail();
+        } catch (error) {
+          expect(error).toEqual({
+            forbidden: `document must have a ${key}`,
+          });
+        }
+      }
+    );
+
+    test.each([DATE_FIELD])('error if %s field is not a date', async (key) => {
+      const doc = { ...newDoc };
+      doc[key] = 1;
+
       try {
-        validate_doc_update(newDoc, {}, {});
+        validate_doc_update(doc, {}, {});
+        fail();
       } catch (error) {
-        expect(error).toEqual({ forbidden: 'title cannot be empty' });
+        expect(error).toEqual({
+          forbidden: `${key} should be a date`,
+        });
       }
     });
+
+    test.each([ITEMS_FIELD])(
+      'error if %s field is not an array',
+      async (key) => {
+        const doc = { ...newDoc };
+        doc[key] = 1;
+
+        try {
+          validate_doc_update(doc, {}, {});
+          fail();
+        } catch (error) {
+          expect(error).toEqual({
+            forbidden: `${key} should be an array`,
+          });
+        }
+      }
+    );
+
+    test.each([
+      CAR_MODEL_FIELD,
+      CAR_PRODUCER_FIELD,
+      CLIENT_NAME_FIELD,
+      CLIENT_PHONE_FIELD,
+    ])('error if %s field is defined, but is empty', async (key) => {
+      const doc = { ...newDoc };
+      doc[key] = '';
+
+      try {
+        validate_doc_update(doc, {}, {});
+        fail();
+      } catch (error) {
+        expect(error).toEqual({
+          forbidden: `${key} cannot be empty`,
+        });
+      }
+    });
+
+    test.each([
+      ORDER_ITEM_CAR_CLASS_FIELD,
+      ORDER_ITEM_COUNT_FIELD,
+      ORDER_ITEM_PART_FIELD,
+      ORDER_ITEM_SIZE_FIELD,
+      ORDER_ITEM_TABLE_FIELD,
+      ORDER_ITEM_PRICE_FIELD,
+    ])('error if %s item field is not defined', async (key) => {
+      const doc = {
+        ...newDoc,
+        items: [{ ...newDoc.items[0] }, { ...newDoc.items[0] }],
+      };
+      delete doc.items[0][key];
+
+      try {
+        validate_doc_update(doc, {}, {});
+        fail();
+      } catch (error) {
+        expect(error).toEqual({
+          forbidden: `item field ${key} should be defined`,
+        });
+      }
+    });
+
+    test.each([ORDER_ITEM_COUNT_FIELD, ORDER_ITEM_PRICE_FIELD])(
+      'error if %s item field is not a number',
+      async (key) => {
+        const doc = {
+          ...newDoc,
+          items: [{ ...newDoc.items[0] }, { ...newDoc.items[0] }],
+        };
+        doc.items[0][key] = '1';
+
+        try {
+          validate_doc_update(doc, {}, {});
+          fail();
+        } catch (error) {
+          expect(error).toEqual({
+            forbidden: `item field ${key} should be a number`,
+          });
+        }
+      }
+    );
   });
 });
--- apps/api/src/app/design/index.ts
+++ apps/api/src/app/design/index.ts
@@ -1,23 +1,97 @@
 export const validate_doc_update = function (newDoc, oldDoc, userCtx) {
-  function require(field, message?) {
-    message = message || 'Document must have a ' + field;
-    if (newDoc[field] === undefined) throw { forbidden: message };
+  // fields
+  const CATEGORY_FIELD = 'category';
+  const DATE_FIELD = 'date';
+  const ITEMS_FIELD = 'items';
+
+  const CAR_MODEL_FIELD = 'carModel';
+  const CAR_PRODUCER_FIELD = 'carProducer';
+  const CLIENT_NAME_FIELD = 'clientName';
+  const CLIENT_PHONE_FIELD = 'clientPhone';
+
+  const ORDER_ITEM_CAR_CLASS_FIELD = 'carClass';
+  const ORDER_ITEM_COUNT_FIELD = 'count';
+  const ORDER_ITEM_PART_FIELD = 'part';
+  const ORDER_ITEM_SIZE_FIELD = 'size';
+  const ORDER_ITEM_TABLE_FIELD = 'table';
+  const ORDER_ITEM_PRICE_FIELD = 'price';
+
+  // values
+  const CATEGORY_VALID_VALUES = [];
+  const CAR_CLASS_VALID_VALUES = [];
+  const PART_VALID_VALUES = [];
+  const SIZE_VALID_VALUES = [];
+
+  // validators
+  function isRequired(doc, field, message?) {
+    message = message || 'document must have a ' + field;
+    if (doc[field] === undefined) throw { forbidden: message };
   }
 
-  function notEmpty(field, message?) {
-    require(field, message);
-
+  function notEmpty(doc, field, message?) {
     message = message || field + ' cannot be empty';
-    if (!newDoc[field].length) throw { forbidden: message };
+    if (doc[field] !== undefined && !doc[field].length) {
+      throw { forbidden: message };
+    }
   }
+
+  function isNumber(doc, field, message?) {
+    message = message || field + ' should be a number';
+    if (typeof doc[field] !== 'number') throw { forbidden: message };
+  }
+
+  function isString(doc, field, message?) {
+    message = message || field + ' should be a string';
+    if (typeof doc[field] !== 'string') throw { forbidden: message };
+  }
+
+  function isArray(doc, field, message?) {
+    message = message || field + ' should be an array';
+    if (!Array.isArray(doc[field])) throw { forbidden: message };
+  }
+
+  function isDate(doc, field, message?) {
+    message = message || field + ' should be a date';
+    if (!(doc[field] instanceof Date)) throw { forbidden: message };
+  }
+
+  // process
 
   if (newDoc._id === '_design/base') {
-    throw { forbidden: 'You cannot update or delete a filter' };
+    throw { forbidden: 'You cannot update or delete the filter' };
   }
 
   if (newDoc.type === 'order') {
-    notEmpty('title');
-    return;
+    [CATEGORY_FIELD, DATE_FIELD, ITEMS_FIELD].forEach((field) =>
+      isRequired(newDoc, field)
+    );
+    [DATE_FIELD].forEach((field) => isDate(newDoc, field));
+    [ITEMS_FIELD].forEach((field) => isArray(newDoc, field));
+    [
+      CAR_MODEL_FIELD,
+      CAR_PRODUCER_FIELD,
+      CLIENT_NAME_FIELD,
+      CLIENT_PHONE_FIELD,
+    ].forEach((field) => notEmpty(newDoc, field));
+
+    newDoc.items.forEach((item) => {
+      [
+        ORDER_ITEM_CAR_CLASS_FIELD,
+        ORDER_ITEM_COUNT_FIELD,
+        ORDER_ITEM_PART_FIELD,
+        ORDER_ITEM_SIZE_FIELD,
+        ORDER_ITEM_TABLE_FIELD,
+        ORDER_ITEM_PRICE_FIELD,
+      ].forEach((field) =>
+        isRequired(item, field, `item field ${field} should be defined`)
+      );
+
+      [ORDER_ITEM_COUNT_FIELD, ORDER_ITEM_PRICE_FIELD].forEach((field) =>
+        isNumber(item, field, `item field ${field} should be a number`)
+      );
+    });
+
+    return true;
   }
 
   throw { forbidden: 'Unknown data' };
